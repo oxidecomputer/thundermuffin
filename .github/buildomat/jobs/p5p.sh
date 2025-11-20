43fa4341bdb8003209af229dd8e19d6dc954654c
Adam Suczewski <adam.suczewski@oxidecomputer.com>
2025-11-20 13:32:27 -0800

    pkg: build thundermuffin pkg in CI

---
+++ .github/buildomat/jobs/p5p.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#:
+#: name = "thundermuffin-p5p"
+#: variety = "basic"
+#: target = "helios-2.0"
+#: rust_toolchain = "stable"
+#: output_rules = [
+#:   "=/out/thundermuffin.p5p",
+#:   "=/out/thundermuffin.p5p.sha256",
+#: ]
+#:
+#: [[publish]]
+#: series = "repo"
+#: name = "thundermuffin.p5p"
+#: from_output = "/out/thundermuffin.p5p"
+#:
+#: [[publish]]
+#: series = "repo"
+#: name = "thundermuffin.p5p.sha256"
+#: from_output = "/out/thundermuffin.p5p.sha256"
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+banner build
+ptime -m cargo build --release
+
+banner package
+cargo run -p thundermuffin-package
+
+banner copy
+pfexec mkdir -p /out
+pfexec chown "$UID" /out
+PKG_NAME="/out/thundermuffin.p5p"
+mv out/*.p5p "$PKG_NAME"
+sha256sum "$PKG_NAME" > "$PKG_NAME.sha256"
